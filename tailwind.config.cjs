b860a2943ae5395fbd87711ec264c5460fe5cb69
mariandotg <marianguillaume.m@gmail.com>
2022-09-09 21:48:16 -0300

    Added basic tailwind theme config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./*.{html,js, ts}', './src/**/*.{html,js, ts}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        base: '8px',
+      },
+      colors: {
+        accent: '#FF6600',
+        background: '#000000',
+        text: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
