f7417dae1ff340e986528fdc36e0386358ee6878
mariandotg <marianguillaume.m@gmail.com>
2022-09-12 06:18:19 -0300

    Added dynamic clips table data

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,33 @@
-const hello = 'Hello world!';
+const clips = [
+  { id: 1, start: 0, end: 62 },
+  { id: 2, start: 5, end: 67 },
+  { id: 2, start: 5, end: 67 },
+];
 
-console.log(hello);
+const clipsRoot = document.getElementById('clips');
+
+clips.map((clip) => {
+  const tr = `
+    <tr>
+      <td>${clip.id}</td>
+      <td>${clip.start}</td>
+      <td>${clip.end}</td>
+      <td>
+        <span class="material-icons" id="play_clip">
+        play_arrow
+        </span>
+      </td>
+      <td>
+        <span class="material-icons" id="download">
+        file_download
+        </span>
+      </td>
+      <td>
+        <span class="material-icons" id="delete">
+        delete
+        </span>
+      </td>
+    </tr>`;
+
+  return (clipsRoot!.innerHTML += tr);
+});
