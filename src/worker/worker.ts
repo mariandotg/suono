8a93b6570e643769f5fc7c0ca69c7687aef7d866
mariandotg <marianguillaume.m@gmail.com>
2022-10-06 21:04:09 -0300

    Added compatibility with mono audios

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -7,6 +7,25 @@ const encode = (audioData: any, emptyBuffer: any[]) => {
     audioData.sampleRate,
     128
   );
+  if (audioData.channels.length === 1) {
+    return new Promise((resolve, reject) => {
+      const arrayBuffer = audioData.channels[0];
+      const data = new Float32Array(arrayBuffer);
+      const samples = new Int16Array(arrayBuffer.length);
+      floatTo16BitPCM(data, samples);
+
+      let remaining = samples.length;
+      for (let i = 0; remaining >= 0; i += 1152) {
+        const mono = samples.subarray(i, i + 1152);
+        const mp3buf = mp3Encoder.encodeBuffer(mono);
+        emptyBuffer.push(new Int8Array(mp3buf));
+        remaining -= 1152;
+      }
+      if (emptyBuffer.length !== 0) resolve(emptyBuffer);
+      else reject(new Error('Buffer error'));
+    });
+  }
+
   return new Promise((resolve, reject) => {
     const right = new Int16Array(audioData.channels[0].length);
     floatTo16BitPCM(audioData.channels[0], right);
